87367b43cf79ee544e05404038659b88f5c40e6e
Bkzazzau1 <y.i.bashir@gmail.com>
2026-09-17 10:46:10 +0100

    Connect hall seat map to shared invigilator demo state

--- lib/modules/invigilator/controller/seat_map_controller.dart
+++ lib/modules/invigilator/controller/seat_map_controller.dart
@@ -1,24 +1,27 @@
 import 'package:get/get.dart';
 
 import '../../../data/models/seat_map_models.dart';
-import '../../../data/services/seat_map_mock_service.dart';
+import '../../../data/services/invigilator_demo_store.dart';
 
 class SeatMapController extends GetxController {
   final isLoading = false.obs;
-  final records = <SeatMapRecord>[].obs;
   final selectedHall = 'Hall A'.obs;
+
+  late final InvigilatorDemoStore _demoStore;
 
   @override
   void onInit() {
     super.onInit();
+    _demoStore = Get.isRegistered<InvigilatorDemoStore>()
+        ? Get.find<InvigilatorDemoStore>()
+        : Get.put(InvigilatorDemoStore(), permanent: true);
     load();
   }
 
   Future<void> load() async {
     isLoading.value = true;
     try {
-      final items = await SeatMapMockService.loadSeatMap();
-      records.assignAll(items);
+      await _demoStore.ensureLoaded();
 
       final halls = hallOptions;
       if (halls.isNotEmpty && !halls.contains(selectedHall.value)) {
@@ -30,14 +33,13 @@ class SeatMapController extends GetxController {
   }
 
   List<String> get hallOptions {
-    final halls = records.map((e) => e.hallName).toSet().toList()..sort();
+    final halls = _demoStore.seats.map((e) => e.hallName).toSet().toList()..sort();
     return halls;
   }
 
   List<SeatMapRecord> get currentHallSeats {
     final hall = selectedHall.value;
-    final items = records.where((e) => e.hallName == hall).toList();
-
+    final items = _demoStore.seats.where((e) => e.hallName == hall).toList();
     items.sort((a, b) => a.seatNumber.compareTo(b.seatNumber));
     return items;
   }
