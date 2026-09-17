f74111855f7e94b2671a351f292556867adcebf7
Bkzazzau1 <y.i.bashir@gmail.com>
2026-09-17 15:29:44 +0100

    Add manual identity review route

--- lib/app/routes/app_routes.dart
+++ lib/app/routes/app_routes.dart
@@ -21,6 +21,7 @@ abstract class Routes {
   static const candidateActionPanel = '/candidate-action-panel';
 
   static const attendanceRegister = '/attendance-register';
+  static const manualIdentityReview = '/manual-identity-review';
   static const seatMap = '/seat-map';
   static const workstationAllocation = '/workstation-allocation';
   static const technicalReports = '/technical-reports';
