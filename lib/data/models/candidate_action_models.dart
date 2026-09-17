f8860afa4fce613dd1e643c23708fe596287d4b6
Bkzazzau1 <y.i.bashir@gmail.com>
2026-09-17 09:59:48 +0100

    Add validated seat reassignment models and audit records

--- lib/data/models/candidate_action_models.dart
+++ lib/data/models/candidate_action_models.dart
@@ -7,6 +7,79 @@ enum CandidateExamControlState {
   seatReassigned,
 }
 
+enum SeatReassignmentReason {
+  workstationFault,
+  networkIssue,
+  powerIssue,
+  peripheralIssue,
+  accessibility,
+  supervisorInstruction,
+  other,
+}
+
+extension SeatReassignmentReasonX on SeatReassignmentReason {
+  String get label {
+    switch (this) {
+      case SeatReassignmentReason.workstationFault:
+        return 'Workstation fault';
+      case SeatReassignmentReason.networkIssue:
+        return 'Network issue';
+      case SeatReassignmentReason.powerIssue:
+        return 'Power issue';
+      case SeatReassignmentReason.peripheralIssue:
+        return 'Keyboard / mouse / display issue';
+      case SeatReassignmentReason.accessibility:
+        return 'Accessibility requirement';
+      case SeatReassignmentReason.supervisorInstruction:
+        return 'Supervisor instruction';
+      case SeatReassignmentReason.other:
+        return 'Other';
+    }
+  }
+
+  bool get marksOldSeatAsTechnicalIssue {
+    switch (this) {
+      case SeatReassignmentReason.workstationFault:
+      case SeatReassignmentReason.networkIssue:
+      case SeatReassignmentReason.powerIssue:
+      case SeatReassignmentReason.peripheralIssue:
+        return true;
+      case SeatReassignmentReason.accessibility:
+      case SeatReassignmentReason.supervisorInstruction:
+      case SeatReassignmentReason.other:
+        return false;
+    }
+  }
+}
+
+class SeatReassignmentRecord {
+  SeatReassignmentRecord({
+    required this.id,
+    required this.candidateName,
+    required this.registrationNumber,
+    required this.hallName,
+    required this.oldSeatNumber,
+    required this.newSeatNumber,
+    required this.oldWorkstationId,
+    required this.newWorkstationId,
+    required this.reason,
+    required this.note,
+    required this.createdAt,
+  });
+
+  final String id;
+  final String candidateName;
+  final String registrationNumber;
+  final String hallName;
+  final String oldSeatNumber;
+  final String newSeatNumber;
+  final String oldWorkstationId;
+  final String newWorkstationId;
+  final SeatReassignmentReason reason;
+  final String note;
+  final DateTime createdAt;
+}
+
 class CandidateActionContext {
   CandidateActionContext({
     required this.workstationId,
